ba78fdf2069b3d3d0169690aa7b1357119be8bd7
AntonioFilipov <tonkatafilipov@gmail.com>
2015-06-21 16:13:06 +0300

    add vector implementation

--- week1/1-Vector/Vector.java
+++ week1/1-Vector/Vector.java
@@ -1,44 +1,82 @@
 public class Vector<T> {
+    
+    private T[] list;
+    private static final int INCREASEMENT_SIZE=2;
+    private static final int INITIAL_CAPACITY=4;
+    private int capacity;
+    private int size;
 
-  // Adds value at a specific index in the Vector.
-  // Complexity: O(n)
-  public void insert(int index, T value) {
-    // ...
-  }
+    public Vector(){
+        this.list = (T[])new Object[this.INITIAL_CAPACITY];
+        this.capacity = this.INITIAL_CAPACITY;
+        this.size=0;
+    }
+    
+    public Vector(int capacity){
+        this.list = (T[])new Object[capacity];
+        this.capacity=capacity;
+        this.size=0;
+    }
+    
+    private void extend(){
+        if (this.capacity == this.size) {
+            this.capacity*=this.INCREASEMENT_SIZE;;
+            T[] copyArray = (T[])new Object[this.capacity];
+            for (int i = 0; i < this.list.length; i++) {
+                copyArray[i]=this.list[i];
+            }
+            this.list = copyArray;
+        }
+    }
+    // Adds value at a specific index in the Vector.
+    // Complexity: O(n)
+    public void insert(int index, T value) {
+        extend();
+        for (int i = this.list.length-1; i >index; i--) {
+            this.list[i] = this.list[i-1];
+        }
+        this.list[index] = value;
+        this.size++;
+    }
 
-  // Adds value to the end of the Vector.
-  // Complexity: O(1)
-  public void add(T value) {
-    // ...
-  }
+    // Adds value to the end of the Vector.
+    // Complexity: O(1)
+    public void add(T value) {
+        extend();
+        this.list[size]=value;
+        this.size++;
+    }
 
-  // Returns value at a specific index in the Vector
-  // Complexity: O(1)
-  public T get(int index) {
-    // ...
-  }
+    // Returns value at a specific index in the Vector
+    // Complexity: O(1)
+    public T get(int index) {
+        return this.list[index];
+    }
 
-  // Removes element at the specific index
-  // Complexity: O(n)
-  public void remove(int index) {
-    // ...
-  }
+//    // Removes element at the specific index
+//    // Complexity: O(n)
+    public void remove(int index) {
+        for (int i = index; i < this.list.length-1; i++) {
+            this.list[i]=this.list[i+1];
+        }
+        this.size--;
+    }
 
-  // Removes element at the last index
-  // Complexity: O(1)
-  public T pop() {
-    // ...
-  }
+//    // Removes element at the last index
+//    // Complexity: O(1)
+    public T pop() {
+        return this.list[this.size-1];
+    }
 
-  // Returns the number of elements in the Vector.
-  // Complexity: O(1)
-  public int size() {
-    // ...
-  }
+//    // Returns the number of elements in the Vector.
+//    // Complexity: O(1)
+    public int size() {
+        return this.size;
+    }
 
-  // Returns the total capacity of the Vector.
-  // Complexity: O(1)
-  public int capacity() {
-    // ...
-  }
+//    // Returns the total capacity of the Vector.
+//    // Complexity: O(1)
+    public int capacity() {
+        return this.capacity;
+    }
 }
