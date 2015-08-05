afb5394085f6bd6f66fb6e515cab53e8491bde10
AntonioFilipov <tonkatafilipov@gmail.com>
2015-08-05 12:42:35 +0300

    add minMaxHeap

--- week3/2-Min-Max-Heap/MinMaxHeap.java
+++ week3/2-Min-Max-Heap/MinMaxHeap.java
@@ -1,14 +1,55 @@
+package week3;
+
+import java.util.Scanner;
+
 public class MinMaxHeap {
+    public static int[] heap;
+    public static int size;
 
-  public static class Node {
-
-    public int value;
-    public Node left;
-    public Node right;
-  }
-
-  // Checks if a binary tree is a min/max heap.
-  public boolean isMinMax(Node root) {
-    // ...
-  }
+    public static int left(int i) {
+        return 2 * i + 1;
+    }
+    
+    public static int right(int i) {
+        return 2 * i + 2;
+    }
+    
+    public static String isMinMax() {
+        return isMinMaxLevel(0, 1) ? "YES" : "NO";
+    }
+    
+    public static boolean isMinMaxLevel(int root, int level) {
+        if (root >= heap.length) {
+            return true;
+        }
+        
+        if (level % 2 != 0) {
+            if (left(root) < size && heap[root] > heap[left(root)]
+                    || right(root) < size && heap[root] > heap[right(root)]) {
+                return false;
+            }
+        } else {
+            if (left(root) < size && heap[root] < heap[left(root)]
+                    || right(root) < size && heap[root] < heap[right(root)]) {
+                return false;
+            }
+        }
+        
+        return isMinMaxLevel(left(root), level + 1) && isMinMaxLevel(right(root), level + 1);
+    }
+    
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        size = scanner.nextInt();
+        heap = new int[size];
+        
+        for (int i = 0; i < size; i++) {
+            heap[i] = scanner.nextInt();
+        }
+        
+        System.out.println(isMinMax());
+        
+        scanner.close();
+    }
 }
